f37a6a777fcf162e47b30e1bcd718b8c8b651912
Xubo Leng <hsupo.leng@gmail.com>
2021-10-25 09:01:11 -0500

    Implement belong-in Interval

--- src/interval.jl
+++ src/interval.jl
@@ -1,11 +1,15 @@
-export Interval, minimum, maximum
+export Interval, minimum, maximum, in, isempty
 
-struct Interval{T<:Real}
-    min :: T
-    max :: T
+mutable struct Interval{T<:Real}
+    min :: Real
+    max :: Real
 end
 Interval(min::T, max::T) where {T<:Real} = Interval{T}(min, max)
 
 Base.minimum(iv::Interval) = iv.min
 Base.maximum(iv::Interval) = iv.max
+
+Base.in(item::Real, iv::Interval) = item >= iv.min && item <= iv.max
+Base.in(iv::Interval) = y::Real -> in(y, iv)
+
 
