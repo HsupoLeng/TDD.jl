8565e83b89b20d39319c435ede6621a69fdead47
Xubo Leng <hsupo.leng@gmail.com>
2021-10-25 05:34:26 -0500

    Implement get_reachable_node via recursion

--- src/graph.jl
+++ src/graph.jl
@@ -1,3 +1,14 @@
-export get_direct_neighbor
+export get_direct_neighbor, get_reachable_node
 
-get_direct_neighbor(graph, node) = union(node, graph[node])
+get_direct_neighbor(graph, node) = Set(union(node, graph[node]))
+
+function get_reachable_node(graph, node, reached = Set([node]))
+    neighbor = get_direct_neighbor(graph, node)
+    for n in neighbor
+        if !in(n, reached)
+            push!(reached, n)
+            get_reachable_node(graph, n, reached)
+        end
+    end
+    return reached
+end
