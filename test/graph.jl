69aa4dc1fbdfc0d040785e252dc2c387354035e5
Xubo Leng <hsupo.leng@gmail.com>
2021-10-25 05:30:08 -0500

    Require neighbor be Set; Add reachable node test

--- test/graph.jl
+++ test/graph.jl
@@ -3,7 +3,6 @@ using Test
 
 @testset "graph.jl" begin
 
-    # 1.1
     undirected_graph = [
         [2, 3],     # list of nodes that can be reached from node 1
         [1],        # list of nodes that can be reached from node 2
@@ -14,13 +13,25 @@ using Test
     directed_graph = [
         [2, 3, 4],     # list of nodes that can be reached from node 1
         [1],        # list of nodes that can be reached from node 2
-        [1],        # ", node 3
+        [],        # ", node 3
         [5],        # ", node 4
         [4]]        # ", node 5
 
-    @test get_direct_neighbor(undirected_graph, 1) == [1, 2, 3]
-    @test get_direct_neighbor(undirected_graph, 4) == [4, 5]
+    # 1.1
+    @test get_direct_neighbor(undirected_graph, 1) == Set([1, 2, 3])
+    @test get_direct_neighbor(undirected_graph, 3) == Set([1, 3])
+    @test get_direct_neighbor(undirected_graph, 4) == Set([4, 5])
 
-    @test get_direct_neighbor(directed_graph, 1) == [1, 2, 3, 4]
-    @test get_direct_neighbor(directed_graph, 4) == [4, 5]
+    @test get_direct_neighbor(directed_graph, 1) == Set([1, 2, 3, 4])
+    @test get_direct_neighbor(directed_graph, 3) == Set([3])
+    @test get_direct_neighbor(directed_graph, 4) == Set([4, 5])
+
+    # 1.2
+    @test get_reachable_node(undirected_graph, 1) == Set([1, 2, 3])
+    @test get_reachable_node(undirected_graph, 3) == Set([1, 2, 3])
+    @test get_reachable_node(undirected_graph, 4) == Set([4, 5])
+
+    @test get_reachable_node(directed_graph, 1) == Set([1, 2, 3, 4, 5])
+    @test get_reachable_node(directed_graph, 3) == Set([3])
+    @test get_reachable_node(directed_graph, 4) == Set([4, 5])
 end
