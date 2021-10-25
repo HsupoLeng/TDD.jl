a4e60aaba8418bbcac43a19b9efcb800473bd69f
Xubo Leng <hsupo.leng@gmail.com>
2021-10-25 09:00:26 -0500

    Add belong-in interval test

--- test/interval.jl
+++ test/interval.jl
@@ -9,4 +9,16 @@ using Test
     @test iv.max == 1
     @test minimum(iv) == 0
     @test maximum(iv) == 1
+
+    # 2.2
+    @test in(0.5, iv) == true
+    @test in(0, iv) == true
+    @test in(1, iv) == true
+    @test in(0-eps(), iv) == false
+    @test in(1+eps(), iv) == false
+    @test_broken 0.5 in iv == true
+    @test ∈(0.5, iv) == true
+    f = in(iv)
+    @test f(0.5) == true
+
 end
