90a8e8233b6a405461636e44f703edecf07f8675
Xubo Leng <hsupo.leng@gmail.com>
2021-10-25 14:40:50 -0500

    Test type promotion; Fix test broken by precedence

--- test/interval.jl
+++ test/interval.jl
@@ -4,9 +4,10 @@ using Test
 @testset "interval.jl" begin
 
     # 2.1
-    iv = Interval(0, 1)
+    iv = Interval(0, 1.0)
     @test iv.min == 0
     @test iv.max == 1
+    @test typeof(iv.min) === typeof(1.0)
     @test minimum(iv) == 0
     @test maximum(iv) == 1
 
@@ -16,7 +17,7 @@ using Test
     @test in(1, iv) == true
     @test in(0-eps(), iv) == false
     @test in(1+eps(), iv) == false
-    @test_broken 0.5 in iv == true
+    @test (0.5 in iv) == true
     @test ∈(0.5, iv) == true
     f = in(iv)
     @test f(0.5) == true
@@ -25,5 +26,6 @@ using Test
     iv_empty = Interval(0, -1)
     @test isempty(iv_empty)
     
-        
+    # 2.4
+
 end
